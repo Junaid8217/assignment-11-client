f6476b857ff41e27315d5e6df38b9934323b33c4
Junaid8217 <junaidhossain77349@gmail.com>
2025-12-09 17:51:40 +0600

    Added the functionality of image

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,17 +4,22 @@ import { AuthContext } from '../provider/AuthProvider';
 import { updateProfile } from 'firebase/auth';
 import auth from '../firebase/firebase.config';
 import { toast } from 'react-toastify';
+import axios from 'axios';
 
 const Register = () => {
 
     const { registerWithEmailPAssword, setUser, user, handleGoogleSignIn } = useContext(AuthContext)
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const email = e.target.email.value;
         const name = e.target.name.value;
         const password = e.target.pass.value;
-        const photoUrl = e.target.photoURL.value;
+        const photoUrl = e.target.photoURL;
+        const file = photoUrl.files[0]
+
+
+
 
         const upperCase = /[A-Z]/;
         const lowerCase = /[a-z]/;
@@ -29,26 +34,39 @@ const Register = () => {
             return alert("Need a lower case")
         }
 
+        //to grab image from device 
+        const res = await axios.post(`https://api.imgbb.com/1/upload?&key=d10fcf86c44edda7407c4d175029945a`, { image: file }, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
 
 
-        registerWithEmailPAssword(email, password)
-            .then((userCredential) => {
+        const mainPhotURL = res.data.data.display_url
 
-                updateProfile(auth.currentUser, {
-                    displayName: name, photoURL: photoUrl
-                }).then(() => {
-                    // console.log(userCredential.user);
-                    setUser(userCredential.user)
-                }).catch((error) => {
-                    console.log(error)
-                });
-                toast("Registration Successful")
-                e.target.reset();
-            })
-            .catch(err => {
-                console.log(err)
-            })
+        if (res.data.success == true) {
+
+            registerWithEmailPAssword(email, password)
+                .then((userCredential) => {
+
+                    updateProfile(auth.currentUser, {
+                        displayName: name, photoURL: mainPhotURL
+                    }).then(() => {
+                        // console.log(userCredential.user);
+                        setUser(userCredential.user)
+                    }).catch((error) => {
+                        console.log(error)
+                    });
+                    toast("Registration Successful")
+                    e.target.reset();
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
     }
+
+
 
     const googleSignUp = () => {
         handleGoogleSignIn()
@@ -84,14 +102,14 @@ const Register = () => {
                             <label className="label">Email</label>
                             <input name='email' type="email" className="input" placeholder="Email" />
                             <label className="label">PhotoURL</label>
-                            <input name='photoURL' type="text" className="input" placeholder="PhotoURL" />
+                            <input name='photoURL' type="file" className="input" placeholder="PhotoURL" />
                             <label className="label">Password</label>
                             <input name='pass' type="password" className="input" placeholder="Password" />
-                            
+
                             <div>
                                 <span>Already have an account? <Link to={'/login'} className='text-blue-700 underline'>Login</Link></span>
                             </div>
-                            <button className="btn btn-neutral mt-4">Login</button>
+                            <button className="btn btn-neutral mt-4">Register</button>
                         </form>
                         <button onClick={googleSignUp} className="btn bg-white text-black border-[#e5e5e5]">
                             <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
